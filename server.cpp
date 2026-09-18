a70143e7a5a95828136689a61d5cada92aeb0eea
PaNNiiiiC <amr.khalid@marun.edu.tr>
2026-09-18 23:08:31 +0300

    initial server socket setup

--- server.cpp
+++ server.cpp
@@ -1,5 +1,11 @@
-//
-// Created by AK on 10/09/2026.
-//
+#include <sys/socket.h>
+#include <netinet/ip.h>
 
-#include "server.h"
+int main() {
+
+    int server_fd=socket(AF_INET,SOCK_STREAM,0);
+    int opt=1;
+    setsockopt(server_fd,SOL_SOCKET,SO_REUSEADDR,&opt,sizeof(opt));
+    
+    return 0;
+}
